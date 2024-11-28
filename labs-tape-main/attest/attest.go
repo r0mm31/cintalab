7517f80233ccbc65400a44fe9789a71371f7a3bc
r0mm31 <90ggrc101205@gmail.com>
2024-11-28 17:31:01 -0600

    agregando otracezzz

--- labs-tape-main/attest/attest.go
+++ labs-tape-main/attest/attest.go
@@ -2,45 +2,28 @@ package attest
 
 import (
 	"fmt"
-	"log"
 
 	"github.com/docker/labs-brown-tape/attest/digest"
 	"github.com/docker/labs-brown-tape/attest/types"
 	"github.com/docker/labs-brown-tape/attest/vcs/git"
 )
 
-// SimplePathCheckerRegistry es una estructura que almacena información sobre el PathChecker.
-type SimplePathCheckerRegistry struct {
-	Path     string
-	Provider string
-	Checker  types.PathChecker
-}
+var (
+	_ types.PathChecker = (*git.PathChecker)(nil)
+)
 
-// NewSimplePathCheckerRegistry crea una nueva instancia de SimplePathCheckerRegistry.
-func NewSimplePathCheckerRegistry(path string, provider string) *SimplePathCheckerRegistry {
-	return &SimplePathCheckerRegistry{
-		Path:     path,
-		Provider: provider,
-	}
-}
-
-// DetectVCS detecta el sistema de control de versiones en la ruta especificada.
-func DetectVCS(path string) (bool, *SimplePathCheckerRegistry, error) {
-	if path == "" {
-		return false, nil, fmt.Errorf("path cannot be empty")
-	}
-
+func DetectVCS(path string) (bool, *PathCheckerRegistry, error) {
 	for _, provider := range map[string]func(string, digest.SHA256) types.PathChecker{
+		// TODO: support other VCS providers
 		git.ProviderName: git.NewPathChecker,
 	} {
 		checker := provider(path, "")
 		ok, err := checker.DetectRepo()
 		if err != nil {
-			log.Printf("Error detecting VCS: %v", err)
 			return false, nil, fmt.Errorf("unable to detect VCS: %w", err)
 		}
 		if ok {
-			registry := NewSimplePathCheckerRegistry(path, provider)
+			registry := NewPathCheckerRegistry(path, provider)
 			if err := registry.init(checker); err != nil {
 				return false, nil, err
 			}
@@ -48,13 +31,4 @@ func DetectVCS(path string) (bool, *SimplePathCheckerRegistry, error) {
 		}
 	}
 	return false, nil, nil
-}
-
-// init inicializa el SimplePathCheckerRegistry.
-func (r *SimplePathCheckerRegistry) init(checker types.PathChecker) error {
-	if checker == nil {
-		return fmt.Errorf("checker cannot be nil")
-	}
-	r.Checker = checker
-	return nil
 }
