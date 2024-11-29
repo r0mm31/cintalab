d0daa60a57443dad053a37a6e97fc2fbb4a01cd3
r0mm31 <90ggrc101205@gmail.com>
2024-11-28 18:27:51 -0600

    agregando otracezzz

--- labs-tape-main/attest/attest_test.go
+++ labs-tape-main/attest/attest_test.go
@@ -5,9 +5,7 @@ import (
 	"os/exec"
 	"testing"
 
-	"github.com/docker/labs-brown-tape/attest/digest"
-	"github.com/docker/labs-brown-tape/attest/types"
-	"github.com/docker/labs-brown-tape/attest/vcs/git" // Asegúrate de importar esto.
+	
 )
 
 // TestDetectVCS prueba la función DetectVCS.
